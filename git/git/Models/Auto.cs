a135693b590fcea5b2f4026321b39bffb7551ae2
Andriy Kavetsky <AndriyKavetsky@gmail.com>
2016-06-16 15:44:26 +0300

    First Commit

---
+++ git/git/Models/Auto.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace git.Models
+{
+    class Auto
+    {
+        public string Model { get; set; }
+        public int Price { get; set; }
+        public int Volume { get; set; }
+        public int AmountOfDoors { get; set; }
+    }
+}
